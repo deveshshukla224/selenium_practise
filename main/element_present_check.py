b5f979b2b3416042459d704f42deff419929e7c1
Devesh <deveshshukla224@gmail.com>
2022-12-23 14:43:45 +0530

    selenium_version_dependency changes

--- main/element_present_check.py
+++ main/element_present_check.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from utilities.handy_wrappers import HandyWrappers
+from handy_wrappers import HandyWrappers
+from selenium.webdriver.firefox.service import Service
 import time
 
 
 class ElementPresentCheck():
 
     def test(self):
-        baseUrl = "https://letskodeit.teachable.com/pages/practice"
-        driver = webdriver.Firefox()
+        baseUrl = "https://courses.letskodeit.com/practice"
+        ff_service = Service(executable_path='../driver/geckodriver-v0.32.0-linux64/geckodriver')
+        driver = webdriver.Firefox(service=ff_service)
         driver.maximize_window()
         driver.implicitly_wait(10)
         hw = HandyWrappers(driver)
@@ -17,7 +19,7 @@ class ElementPresentCheck():
         elementResult1 = hw.isElementPresent("name", By.ID)
         print(str(elementResult1))
 
-        elementResult2 = hw.elementPresenceCheck("//input[@id='name1']", By.XPATH)
+        elementResult2 = hw.elementPresenceCheck("//input[@id='name']", By.XPATH)
         print(str(elementResult2))
 
 
