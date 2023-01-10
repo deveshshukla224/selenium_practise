aae4aa975ed358691bead823be8c8f061ea6a028
Devesh <deveshshukla224@gmail.com>
2023-01-10 10:17:00 +0530

    screenshot

--- main/scr_1.py
+++ main/scr_1.py
@@ -2,23 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+
 class Click_Check():
     def test(self):
-        baseURL = "https://letskodeit.teachable.com/"
+        baseURL = "https://www.google.com/"
         cd_service = Service(executable_path='../driver/chromedriver_linux64/chromedriver')
         driver = webdriver.Chrome(service=cd_service)
         driver.maximize_window()
         driver.get(baseURL)
         driver.implicitly_wait(10)
         print(driver.current_url)
-        login_link = driver.find_element(By.XPATH, "//a[contains(text(),'Login')]")
-        login_link.click()
-        emailField = driver.find_element(By.XPATH, "//input[@id='email']")
-        emailField.send_keys("test")
-        time.sleep(5)
-        passwordField = driver.find_element(By.XPATH, "//input[@id='password']")
-        time.sleep(5)
-        passwordField.send_keys("test")
-        time.sleep(5)
+        input_field = driver.find_element(By.XPATH, "//input[@title='Search']")
+        time.sleep(4)
+        text='lucknow'
+        input_field.send_keys(text)
+        destination_file="/home/devslane-75/PycharmProjects/selenium_practise/scr/test1.png"
+        try:
+            driver.save_screenshot(destination_file)
+        except:
+            print("error")
 ff_test = Click_Check()
 ff_test.test()
---
+++ main/scr_generic_method.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+class Click_Check():
+    def test(self):
+        baseURL = "https://www.google.com/"
+        cd_service = Service(executable_path='../driver/chromedriver_linux64/chromedriver')
+        driver = webdriver.Chrome(service=cd_service)
+        driver.maximize_window()
+        driver.get(baseURL)
+        driver.implicitly_wait(10)
+        input_field = driver.find_element(By.XPATH, "//input[@title='Search']")
+        time.sleep(4)
+        text='lucknow'
+        input_field.send_keys(text)
+        self.take_screenshot(driver)
+
+
+    def take_screenshot(self,driver):
+            filename=str(round(time.time()*1000))+".png"
+            scr_dir="/home/devslane-75/PycharmProjects/selenium_practise/scr"
+            destination_file=scr_dir+filename
+            try:
+                driver.save_screenshot(destination_file)
+                print(f'file saved in dir : {scr_dir}')
+            except:
+                print("Error")
+
+
+
+ff_test = Click_Check()
+ff_test.test()
